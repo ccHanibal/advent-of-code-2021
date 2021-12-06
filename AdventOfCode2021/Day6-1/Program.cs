d137da45dbbba4c5e5e79e2117f5375648a20910
Oliver Daum <oliver.daum@codecentric.de>
2021-12-06 12:17:02 +0100

    Implements day 6

---
+++ AdventOfCode2021/Day6-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day6_1
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var fishes = (await File.ReadAllTextAsync("RealData.txt"))
+							.Split(',')
+							.Select(int.Parse)
+							.ToList();
+
+			for (int a = 1; a <= 80; a++)
+			{
+				int newFishes = 0;
+
+				for (int fIdx = 0; fIdx < fishes.Count; fIdx++)
+				{
+					if (fishes[fIdx] == 0)
+					{
+						fishes[fIdx] = 6;
+						newFishes++;
+					}
+					else
+					{
+						fishes[fIdx] = fishes[fIdx] - 1;
+					}
+				}
+
+				fishes.AddRange(Enumerable.Repeat(8, newFishes));
+			}
+
+			Console.WriteLine("How many lanternfish would there be after 80 days?");
+			Console.WriteLine(fishes.Count);
+		}
+	}
+}
---
+++ AdventOfCode2021/Day6-2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day6_2
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var fishes = (await File.ReadAllTextAsync("RealData.txt"))
+							.Split(',')
+							.Select(int.Parse)
+							.GroupBy(t => t)
+							.Select(g => new EquivalentFishes(g.Key, g.Count()))
+							.ToList();
+
+			for (int a = 1; a <= 256; a++)
+			{
+				var newFishes = fishes.Select(f => f.Replicate())
+									.Where(f => f is not null)
+									.ToList();
+
+				if (newFishes.Any())
+					fishes.Add(new EquivalentFishes(8, newFishes.Select(f => f.NumFishes).Sum()));
+			}
+
+			Console.WriteLine("How many lanternfish would there be after 256 days?");
+			Console.WriteLine(fishes.Select(f => f.NumFishes).Sum());
+		}
+
+		public class EquivalentFishes
+		{
+			public long NumFishes { get; private set; }
+			public int TimeTilReplication { get; private set; }
+
+			public EquivalentFishes(int timeTilReplication, long numFishes)
+			{
+				this.TimeTilReplication = timeTilReplication;
+				this.NumFishes = numFishes;
+			}
+
+			public EquivalentFishes Replicate()
+			{
+				if (TimeTilReplication == 0)
+				{
+					TimeTilReplication = 6;
+					return new EquivalentFishes(8, NumFishes);
+				}
+
+				TimeTilReplication--;
+				return null;
+			}
+		}
+	}
+}
