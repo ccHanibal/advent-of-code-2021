7067112bf23275bf19e9c207b5ecd8f6da29235f
Oliver Daum <oliver.daum@codecentric.de>
2021-12-07 11:37:29 +0100

    Implements day 7

---
+++ AdventOfCode2021/Day7-1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day7_1
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var crabs = (await File.ReadAllTextAsync("RealData.txt"))
+									.Split(',')
+									.Select(int.Parse)
+									.GroupBy(i => i)
+									.Select(g => new Crabs { Height = g.Key, NumCrabs = g.Count() })
+									.OrderBy(c => c.Height)
+									.ToList();
+
+			int fuel = 0;
+
+			while (crabs.Any(h => h.Height != crabs[0].Height))
+			{
+				var firstGroup = crabs[0];
+				var lastGroup = crabs[crabs.Count - 1];
+
+				if (firstGroup.NumCrabs <= lastGroup.NumCrabs)
+				{
+					firstGroup.Height++;
+					fuel += firstGroup.NumCrabs;
+
+					var nextGroup = crabs[1];
+
+					if (firstGroup.Height == nextGroup.Height)
+					{
+						// merge groups of same height
+						nextGroup.NumCrabs += firstGroup.NumCrabs;
+						crabs.RemoveAt(0);
+					}
+				}
+				else
+				{
+					lastGroup.Height--;
+					fuel += lastGroup.NumCrabs;
+
+					var secondLastGroup = crabs[^2];
+
+					if (lastGroup.Height == secondLastGroup.Height)
+					{
+						// merge groups of same height
+						secondLastGroup.NumCrabs += lastGroup.NumCrabs;
+						crabs.RemoveAt(crabs.Count - 1);
+					}
+				}
+			}
+
+			Console.WriteLine("How much fuel must they spend to align to that position?");
+			Console.WriteLine("Positon: {0}", crabs[0].Height);
+			Console.WriteLine("Fuel: {0}", fuel);
+		}
+
+		private class Crabs
+		{
+			public int NumCrabs { get; set; }
+			public int Height { get; set; }
+
+			public override string ToString()
+			{
+				return $"Height: {Height}, Num: {NumCrabs}";
+			}
+		}
+	}
+}
---
+++ AdventOfCode2021/Day7-2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day7_2
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var allCrabs = (await File.ReadAllTextAsync("RealData.txt"))
+										.Split(',')
+										.Select(p => new Crab(int.Parse(p)))
+										.OrderBy(c => c.Height)
+										.ToList();
+
+			int fuel = 0;
+
+			while (allCrabs.Any(h => h.Height != allCrabs[0].Height))
+			{
+				var groups = allCrabs.GroupBy(c => c.Height)
+								.Select(g => new { Height = g.Key, Crabs = g.ToList() })
+								.OrderBy(c => c.Height)
+								.ToList();
+
+				var firstGroup = groups[0];
+				var lastGroup = groups[^1];
+				var fuelFirstGroup = CalcFuelOneMove(firstGroup.Crabs);
+				var fuelLastGroup = CalcFuelOneMove(lastGroup.Crabs);
+
+				if (fuelFirstGroup <= fuelLastGroup)
+				{
+					firstGroup.Crabs.ForEach(c => c.Move(true));
+					fuel += fuelFirstGroup;
+				}
+				else
+				{
+					lastGroup.Crabs.ForEach(c => c.Move(false));
+					fuel += fuelLastGroup;
+				}
+
+				static int CalcFuelOneMove(IEnumerable<Crab> crabs)
+				{
+					return crabs.Select(c => c.Moves + 1).Sum();
+				}
+			}
+
+			Console.WriteLine("How much fuel must they spend to align to that position?");
+			Console.WriteLine("Positon: {0}", allCrabs[0].Height);
+			Console.WriteLine("Fuel: {0}", fuel);
+		}
+
+		private class Crab
+		{
+			public int Moves { get; private set; } = 0;
+			public int Height { get; private set; }
+
+			public Crab(int height)
+			{
+				this.Height = height;
+			}
+
+			public void Move(bool up)
+			{
+				if (up)
+					Height++;
+				else
+					Height--;
+
+				Moves++;
+			}
+		}
+	}
+}
